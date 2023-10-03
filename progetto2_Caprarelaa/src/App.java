47124cd2fc4203658f2c547999e57a9a82eedaee
CaprarellaRiccaro <128700856+CaprarellaRiccaro@users.noreply.github.com>
2023-10-03 09:29:25 +0200

    Add files via upload

---
+++ progetto2_Caprarelaa/src/App.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        try{
+            Socket s = new Socket("localHost", 3000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        
+            out.writeBytes("Ciao a tutti" + '\n');
+            String risposta = in.readLine();
+
+            System.out.println("Il server ha detto:" + risposta);
+
+            s.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("connection went wrong, closing client ...");
+            System.exit(1);
+        }
+    }
+}
